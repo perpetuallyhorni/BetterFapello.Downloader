72fc491d24f390e6a90f3453810d3c7dc06dd600
perpetuallyhorni <150314916+perpetuallyhorni@users.noreply.github.com>
2023-11-08 22:04:23 -0600

    Create prep-env.sh

---
+++ prep-env.sh
@@ -0,0 +1,12 @@
+python -m venv bfd 
+source bfd/bin/activate
+pip install -r requirements.txt
+echo ''
+echo 'NOTES:'
+echo "You need to install tk, it can be done via pip 'pip install tk' but on arch it needs to be installed from pacman 'sudo pacman -S tk'"
+echo ''
+echo "Activate the environment by typing 'source bfd/bin/activate'"
+echo ''
+echo "To run the program, type 'python BetterFapelloDownloader.py'"
+echo ''
+echo ''
